e26db6132f027e492d673a49103918934beea2df
domske <domske@mailbox.org>
2018-08-21 02:26:37 +0200

    single uuid

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 let uuidString = document.querySelector('.uuid.string');
 let uuidBytes = document.querySelector('.uuid.bytes');
 
-uuidString.innerText = new UUID().toString();
-uuidBytes.innerText = '[' +
-  new UUID()
-  .toBytes()
-  .map(b => '0x' + ('00' + b.toString(16)).slice(-2).toUpperCase())
-  .join(', ') +
-  ']';
+let uuid = new UUID();
+
+uuidString.innerText = uuid.toString();
+uuidBytes.innerText = '[' + uuid.toBytes().map(b => '0x' + ('00' + b.toString(16)).slice(-2).toUpperCase()).join(', ') + ']';
