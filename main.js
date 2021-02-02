505d635cc0902cd7967d5b9c58f4b4b2586b937e
Dominik Geng <domske@mailbox.org>
2021-02-02 01:06:51 +0100

    updated

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let uuidString = document.querySelector('.uuid.string');
 let uuidBytes = document.querySelector('.uuid.bytes');
 
-let uuid = new UUID();
+let uuid = new Uuid();
 
 uuidString.innerText = uuid.toString();
 uuidBytes.innerText = '[' + uuid.toBytes().map(b => '0x' + ('00' + b.toString(16)).slice(-2).toUpperCase()).join(', ') + ']';
